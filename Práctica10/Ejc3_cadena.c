74875240c510e4e424430448c489b6c20b67764c
Algezz <jgonzg21@estudiantes.unileon.es>
2024-04-13 16:41:30 +0200

    Intento ejc 3

---
+++ Práctica10/Ejc3_cadena.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+void invierte(char *cadenaOrigen,char *cadenaDestino);
+
+int main(){
+    char *cadena1;
+    char *cadena2;
+    cadena1=(char *)malloc(sizeof(char)*100);
+    cadena2=(char *)malloc(sizeof(char)*100);
+    fgets(cadena1,100,stdin);
+    invierte(cadena1, cadena2);
+    printf("%s\n",cadena2);
+    return 0;
+}
+
+void invierte(char *cadenaOrigen,char *cadenaDestino){
+    int i,j,count=0;
+    for (i=0;cadenaOrigen[i]!='\0';i++){
+        count++;
+    }
+    for (j=0;cadenaOrigen[j]!='\0';j++){
+        cadenaDestino[j]=cadenaOrigen[count-j];
+    }
+    cadenaDestino[count+1]='\0';
+}
