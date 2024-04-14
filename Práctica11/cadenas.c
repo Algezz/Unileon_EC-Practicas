c5f96fc7c2488af95f360d55215b942952d00f83
Algezz <jgonzg21@estudiantes.unileon.es>
2024-04-14 11:09:14 +0200

    Try 2

--- Práctica11/cadenas.c
+++ Práctica11/cadenas.c
@@ -15,12 +15,13 @@ int main(int argc, char*argv[]){
     fgets(cadena,100,stdin);
     int i,j,x=0,contiene=0,count1=0,count2=0,count3=0;
 
-    // Contamos la longitud de la cadena y la mostramos por pantalla
+    // Contamos la longitud de la cadena introducida por teclado y la mostramos por pantalla
     for(i=0;cadena[i]!='\0';i++){
         count1++;
     }
     printf("La longitud de la cadena indroducida por teclado es %d\n",count1);
 
+    // Contamos la longitud de la cadena introducida por parámetro y la mostramos por pantalla
     for(i=0;argv[1][i]!='\0';i++){
         count2++;
     }
@@ -39,23 +40,29 @@ int main(int argc, char*argv[]){
 
 
     // Buscamos si la cadena introducida por pantalla contiene el parámetro
-    if(count1>count3){
-        for(i=0;cadena[i]!='\0';i++){
-            if(cadena[i]==argv[1][0]){
-                for(j=1;argv[1][j]!='\0'||x!=0 || count1-1<j;j++){
-                    if(argv[1][j]!=cadena[i+j]){
-                        x++;
+    int dif=count1-count2;
+    if(dif==0){
+        for(i=0;i<=count1;i++){
+            if(cadena[i]!=argv[1][i]){
+                x=1;
+            }
+        }
+        if(x==1) contiene=0;
+    }else if (dif>0){
+        for(i=0;i<dif;i++){
+            if(cadena[i]==argv[1][i]){
+                for(j=i;j<=count2;j++){
+                    if(cadena[j]!=argv[1][j]){
+                        x=1;
                     }
                 }
-                if(x=0){
-                    contiene=1;
-                }
+                if(x==1){contiene=0;} else {contiene=1;}
                 x=0;
             }
         }
     }
     
-
+    // Enviamos un mensaje diciendo si contiene la cadena o no
     if(contiene==1){
         printf("La cadena %s SI contiene %s",cadena,argv[1]);
     }else{
