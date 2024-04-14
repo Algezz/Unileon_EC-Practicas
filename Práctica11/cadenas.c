144e48843b46d1ce9c74bc59db8f90644cacf53d
Algezz <146721710+Algezz@users.noreply.github.com>
2024-04-14 19:24:14 +0200

    Ejc 1 Práctica 11 completo

--- Práctica11/cadenas.c
+++ Práctica11/cadenas.c
@@ -13,12 +13,14 @@ int main(int argc, char*argv[]){
 
     //Recogemos datos
     fgets(cadena,100,stdin);
-    int i,j,x=0,contiene=0,count1=0,count2=0,count3=0;
+    int i,j,count1=0,count2=0,count3=0;
 
     // Contamos la longitud de la cadena introducida por teclado y la mostramos por pantalla
     for(i=0;cadena[i]!='\0';i++){
         count1++;
     }
+    cadena[count1-1]='\0';
+    count1--;
     printf("La longitud de la cadena indroducida por teclado es %d\n",count1);
 
     // Contamos la longitud de la cadena introducida por parámetro y la mostramos por pantalla
@@ -30,44 +32,48 @@ int main(int argc, char*argv[]){
     // Concatenamos las cadenas y las mostramos por pantalla
     for(i=0;cadena[i]!='\0';i++){
         cadenaConcatenada[i]=cadena[i];
-        count3++;
     }
-    count3--;
     for(i=0;argv[1][i]!='\0';i++){
-        cadenaConcatenada[count3+i]=argv[1][i];
+        cadenaConcatenada[count1+i]=argv[1][i];
     }
     printf("La cadena concatenada es:\n%s\n",cadenaConcatenada);
 
 
     // Buscamos si la cadena introducida por pantalla contiene el parámetro
-    int dif=count1-count2;
+    int dif=count1-count2,x=0,contiene=1,z=0;
     if(dif==0){
-        for(i=0;i<=count1;i++){
-            if(cadena[i]!=argv[1][i]){
-                x=1;
-            }
+        for(i=0;cadena[i]!='\0';i++){
+            if(cadena[i]!=argv[1][i])   contiene=0;
         }
-        if(x==1) contiene=0;
     }else if (dif>0){
-        for(i=0;i<dif;i++){
-            if(cadena[i]==argv[1][i]){
-                for(j=i;j<=count2;j++){
-                    if(cadena[j]!=argv[1][j]){
-                        x=1;
+        contiene=0;
+        for(i=0;i<=dif;i++){
+            if(cadena[i]==argv[1][0]){
+                z=0;
+                for(j=i;j<count2;j++){
+                    if(cadena[j]!=argv[1][z]){
+                        x++;
                     }
+                    z++;
                 }
-                if(x==1){contiene=0;} else {contiene=1;}
+                if(x==0)contiene=1;
                 x=0;
+                if(contiene==1)i=dif;
             }
         }
+    }else{
+        contiene=0;
     }
     
     // Enviamos un mensaje diciendo si contiene la cadena o no
     if(contiene==1){
-        printf("La cadena %s SI contiene %s",cadena,argv[1]);
+        printf("La cadena %s SI contiene %s\n",cadena,argv[1]);
     }else{
-        printf("La cadena %s NO contiene %s",cadena,argv[1]);
+        printf("La cadena %s NO contiene %s\n",cadena,argv[1]);
     }
 
+    // Buscamos la letra que ocupa la posicion especificada por parámetro
+
+    printf("La letra en la posición %d es %c\n",atoi(argv[2]),cadena[atoi(argv[2])-1]);
     return 0;
 }
